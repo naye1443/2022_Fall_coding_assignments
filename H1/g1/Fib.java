2c3cbc6a96fe9dfeafb2ed9217ff3e565d616b7d
naye1443 <eyaneubanks@gmail.com>
2022-08-30 16:12:24 -0400

    adding current work on H1/g1

---
+++ H1/g1/Fib.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Fib{
+
+  //Constructor
+  public Fib(/*int f0, int f1*/){
+  }
+
+  //Fibonacci numbers iteratively
+  int fib_itr(int n){
+
+    Scanner in = new Scanner(System.in);
+    n = in.nextInt();
+
+    if (n < 0){
+      try{
+        throw new ArithmeticException("This number is not greater than 0.");
+      } catch (ArithmeticException e){
+        System.out.println("You can not have a number lower than 0");
+      }
+    }else{
+      System.out.println("This number is greater than 0");
+    }
+
+    
+
+    return 0;
+  }
+
+  //Fibonacci numbers recursively
+
+  //Main function to call different versions for Fibonacci number calculation
+  public static void main(String[] args){
+    System.out.println("This is a cool funciton, lets get this started");
+    Fib tempfib = new Fib();
+    tempfib.fib_itr(-3);
+
+    System.out.println("This is an exception");
+    return;
+  }
+};
