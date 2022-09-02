ceeecb066ad6ad69d539b047ccdf06e748c8a77d
naye1443 <eyaneubanks@gmail.com>
2022-09-02 01:37:55 -0400

    Implmented Recursive function for Fibonacci Sequence

--- H1/g1/Fib.java
+++ H1/g1/Fib.java
@@ -10,7 +10,7 @@ public class Fib{
   // Fibonacci numbers iteratively
   int fib_itr(int n){
 
-    // Creats needed variables as BigIntegers
+    // Creates needed variables as BigIntegers
     int i = 0;
     BigInteger F_1 = new BigInteger("0");
     BigInteger F_2 = new BigInteger("1");
@@ -33,6 +33,28 @@ public class Fib{
   }
 
   // Fibonacci numbers recursively
+  int fib_recur(int n){
+    // Sets F(1) and F(2) equal to -1(null)
+    int F_1 = -1;
+    int F_2 = -1;
+
+    // Base case
+    if( n == 0){
+      return 0;
+    }else if(n == 1){ // Base case
+      return 1;
+    }else{  // Retermine F_1 and F_2 by recursively checking each possiblitity
+      if(F_1 == -1){
+        F_1 = fib_recur(n-1);
+      }
+      if(F_2 == -1){
+        F_2 = fib_recur(n-2);
+      }
+    }
+    System.out.println("The Fibonacci number at i = " + n + " is " + (F_1 + F_2));
+    return (F_1 + F_2); // Return computed sum back to called function
+  }
+  // To make this faster, could implment Dynamic programming to remember already calculated results.
 
   // Takes user input
   public int userInput(){
@@ -78,6 +100,7 @@ public class Fib{
     int input = tempfib.userInput();
 
     tempfib.fib_itr(input);
+    tempfib.fib_recur(input);
 
     System.out.println("End of main function");
     return;
