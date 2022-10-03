c75264a00b6198a1112e2e24086d46561f779a55
naye1443 <eyaneubanks@gmail.com>
2022-10-03 09:31:24 -0400

    Refactored Gui_square program

--- H3/Gui_square/src/Frame.java
+++ H3/Gui_square/src/Frame.java
@@ -4,53 +4,82 @@ import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 
-public class Frame {
-
+public class Frame{
+    /**
+     * Is Entry point into program
+     * @param args
+     */
     public static void main(String[] args){
 
         JFrame frame = new JFrame();
+        Frame myframe = new Frame();
 
         frame.setVisible(true); // set to visible
 
-        // set layout
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(new FlowLayout()); // set layout
+
+
 
         // add rectangle for the label
-        // add 3 buttons for the label
-        JButton yellow = new JButton("YellowButton");
-        JButton Red = new JButton("RedButton");
-        JButton Blue = new JButton("BlueButton");
-
         Rectangle MyRect = new Rectangle(ICON_WIDTH,ICON_HEIGHT,ICON_SIZE);
         JLabel label = new JLabel(MyRect);
 
-        Red.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e){
-                MyRect.changeColor(Color.RED);// change color
-                label.repaint();
-            }
-        });
+        // add 3 buttons for the label
+        JButton Buttons[] = new JButton[3];
+        for(int i = 0; i < 3; i++){
+            final Color c = myframe.Colors[i];
+            Buttons[i] = myframe.createButton(i, MyRect, label);
+        }
 
-
-
-
+        // add buttons and labels
         frame.add(label);
-        frame.add(yellow);
-        frame.add(Red);
-        frame.add(Blue);
-
+        for(int i = 0; i < 3; i++){
+            frame.add(Buttons[i]);
+        }
 
         frame.pack();   // sets smallest size to display components
 
-        // set on close condition
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set on close condition
 
-
-        // listen to button changes
     }
+
+    /**
+     * This function takes the index of an array. The button with the related actionlistener is returned
+     * @param i
+     * @return
+     */
+    public JButton createButton(int i, Rectangle MyRect, JLabel label){
+        String colorname = "";
+        final Color colorchoice = Colors[i];
+        if(i == 0) {
+            colorname = "Red";
+        }
+        else if(i == 1) {
+            colorname = "Blue";
+        }
+        else{
+            colorname = "Yellow";
+        }
+        JButton newButton = new JButton(colorname);
+        newButton.addActionListener(createchangeColorListener(colorchoice, MyRect, label));
+        return newButton;
+    }
+
+    // helper method for adding button listeners
+    public static ActionListener createchangeColorListener(Color C, Rectangle MyRect, JLabel label){
+        return new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e){
+                MyRect.changeColor(C);  // change color
+                label.repaint();
+            }
+        };
+    }
+
     // pixel measurements
-    private static final int ICON_WIDTH = 400;
+    private static final int ICON_WIDTH = 100;
     private static final int ICON_HEIGHT = 100;
-    private static final int ICON_SIZE = 100;
+    private static final int ICON_SIZE = 10000;
+
+    public Color[] Colors = {Color.RED, Color.BLUE, Color.YELLOW};
 }
--- H3/Gui_square/src/Rectangle.java
+++ H3/Gui_square/src/Rectangle.java
@@ -3,7 +3,14 @@ import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
 public class Rectangle implements Icon{
-
+    /**
+     * @precondition none
+     * @postcondition new Rectangle Icon that's color is yellow
+     * Initiates Rectangle with width, height, size, and rectangle color to yellow
+     * @param width, width of object in pixels
+     * @param height, height of object in pixels
+     * @param size, area of object in pixels
+     */
     public Rectangle(int width, int height, int size){
         this.width = width;
         this.height = height;
@@ -11,21 +18,38 @@ public class Rectangle implements Icon{
         this.CurrColor = Color.yellow;
     }
 
+    /**
+     * @precondition, none
+     * @postcondition, Current Icons height is returned
+     * @return, returns Icons height in pixels
+     */
     public int getIconHeight(){
         return height;
     }
-
+    /**
+    * @precondition, none
+    * @postcondition, Current Icons width is returned
+    * @return, returns Icons width in pixels
+    */
     public int getIconWidth(){
         return width;
     }
-
+    /**
+     * @precondition, the icon has a color, (initially yellow)
+     * @postcondition, the icons color will be changed to the given Color value
+     * @return, none
+     */
     public void changeColor(Color C){
         this.CurrColor = C;
     }
-
+    /**
+     * @precondition, Icon needs to be initalized
+     * @postcondition, The Icon needs to be painted and held within a Jlabel object
+     * @return, none
+     */
     public void paintIcon(Component c, Graphics g, int x, int y){
         Graphics2D g1 = (Graphics2D) g; // typecast abstract graphics to 2D graphics
-        Rectangle2D.Double box = new Rectangle2D.Double(x, y, size, size);  // create rectangle
+        Rectangle2D.Double box = new Rectangle2D.Double(x, y, width, height);  // create rectangle
         g1.setColor(CurrColor);
         g1.fill(box);
     }
